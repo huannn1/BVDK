364c9b446a272aceed8c51f683afc89a273f77e9
Huan Ngo Ngoc <lixboi@XILXIL.local>
2018-06-07 00:03:22 +0700

    Second

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    gulp.src('styles/*.scss')
+    gulp.src('src/styles/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
@@ -11,5 +11,5 @@ gulp.task('sass', function() {
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('styles/*.scss', ['sass']);
+    gulp.watch('src/styles/*.scss', ['sass']);
 })
